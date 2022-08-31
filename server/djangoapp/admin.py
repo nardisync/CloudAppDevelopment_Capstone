d6d357b5ab1458919e2a2e3a82af7e23dafdd71e
nardisync <nardi.sync@gmail.com>
2022-08-31 19:54:11 +0000

    Added Classes for the models and the Admin site

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
-# Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('car_maker', 'car_name')
+    list_filter = ['car_year', 'car_type', 'car_maker']
+    search_fields = ['car_name', 'car_type']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name']
+
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
